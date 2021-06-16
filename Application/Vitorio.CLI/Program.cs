08781f1e423ef0fe6c71d647c1b43a88fcd69481
Vitor de Souza <vitordesolza@gmail.com>
2021-06-16 20:42:52 -0300

    :smile:

---
+++ Application/Vitorio.CLI/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.Extensions.CommandLineUtils;
+
+var console = new CommandLineApplication
+{
+    Name = "vitorio-cli",
+    Description = "CLI for joking",
+    AllowArgumentSeparator = true
+};
+
+console.HelpOption("-? | -h | --help");
+
+console.Command("hello", target =>
+{
+    var name = target.Argument("name", "Say a name");
+    target.OnExecute(() =>
+    {
+        Console.WriteLine($"Hello {name.Values[0]}");
+        return 0;
+    });
+});
+
+console.OnExecute(() =>
+{
+    console.ShowHelp();
+    return 1;
+});
+
+console.Execute(args);
