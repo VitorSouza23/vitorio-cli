5e993fe713840d54da954d2c60f2b1d9eed04cb9
Vitor de Souza <vitordesolza@gmail.com>
2021-06-19 16:27:14 -0300

    Adição do comando About

---
+++ Application/Vitorio.CLI/Commands/About/AboutCommand.cs
@@ -0,0 +1,24 @@
+using System.CommandLine;
+using System.CommandLine.Invocation;
+
+namespace Vitorio.CLI.Commands.About
+{
+    public class AboutCommand : ICommandFactory
+    {
+        public Command Create()
+        {
+            Command command = new("about", "Sobre essa CLI");
+
+            command.Handler = CommandHandler.Create((IConsole console) =>
+            {
+                console.Out.Write(@"Essa CLI foi baseada em minhas necessidades para geração e manipulação de dados durante o trabalho.
+Todos os dados gerados e manipulados por ela são de total responsabilidade de quem as usa.
+Toda ajuda e sugestão são bem-vidas.
+(っ▀¯▀)つ");
+                console.Out.Write("\n");
+            });
+
+            return command;
+        }
+    }
+}
--- Application/Vitorio.CLI/Program.cs
+++ Application/Vitorio.CLI/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System.CommandLine;
+using Vitorio.CLI.Commands.About;
 using Vitorio.CLI.Commands.Gen;
 
 RootCommand rootCommand = new("CLI com ferramentas que podem (ou não) serem úteis para o dia a dia do desenvolvedor");
+rootCommand.AddCommand(new AboutCommand().Create());
 rootCommand.AddCommand(new GenCommand().Create());
 
 return rootCommand.InvokeAsync(args).Result;
