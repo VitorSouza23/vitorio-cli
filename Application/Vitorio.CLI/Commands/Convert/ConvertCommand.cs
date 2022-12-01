646844da8ef04f31c9976b6d1e465db553071694
Vitor de Souza <vitordesolza@gmail.com>
2022-12-01 12:08:18 -0300

    Letting Convert to Base 64 more dinamic

--- Application/Vitorio.CLI/Commands/Convert/ConvertCommand.cs
+++ Application/Vitorio.CLI/Commands/Convert/ConvertCommand.cs
@@ -5,7 +5,7 @@ public class ConvertCommand : ICommandFactory
     public Command Create()
     {
         Command command = new("convert", "Converte um dado de um tipo para outro");
-        command.AddCommand(new ConvertImageToBae64Command().Create());
+        command.AddCommand(new ConvertToBae64Command().Create());
         return command;
     }
 }
